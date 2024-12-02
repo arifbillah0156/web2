8a2e5cbf9d91bfb5da1c8e431f11503ee5cd1106
arifbillah0156 <arifbillah0156@gmail.com>
2024-12-02 13:25:36 +0600

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import HomePageText from "@/components/HomePageText";
+
+export default function Home() {
+  return (
+    <div>
+      <HomePageText />
+    </div>
+  );
+}
---
+++ components/HomePageText.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import img from "@/public/Images/Text BG.jpg";
+import Image from "next/image";
+export default function HomePageText() {
+  return (
+    <div>
+      <div className="relative w-full h-40 ">
+        {/* Background Image */}
+        <Image
+          src={img}
+          alt="Background"
+          layout="fill"
+          className="rounded-3xl"
+        />
+
+        {/* Text Overlay */}
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="text-center text-white underline underline-offset-8">
+            <h1 className="text-4xl md:text-6xl animate-bounce">
+              দ্বীনদার পাত্র/পাত্রীর সন্ধান
+            </h1>
+          </div>
+        </div>
+      </div>
+
+      {/* উপদেশ */}
+      <div
+        className="p-2 bg-[#FBFBFB] shadow-lg shadow-pink-100 rounded-full"
+        title="আপনি বিয়ে করতে চাইলে আমরা আপনাকে কিছুটা সাহায্য করতে পারি।😉"
+      >
+        <p className="text-2xl sm:text-4xl p-4  font-bold  text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 animate-pulse text-center ">
+          {" "}
+          “আপনি অবিবাহিত হলে বিবাহ করে অর্ধেক দ্বীন পূরন করুন, বাকি অর্ধেকের
+          জন্য আল্লাহকে ভয় করুন।”
+        </p>
+      </div>
+    </div>
+  );
+}
